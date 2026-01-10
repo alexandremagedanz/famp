1ec40a10e2ee7a7ccdaf2caca06b0f2379f47420
Alexandre Magedanz <alexandre.magedanz@gmail.com>
2026-01-10 17:44:09 -0300

    Conclusão Seção 5

--- secao05/core/configs.py
+++ secao05/core/configs.py
@@ -2,11 +2,20 @@ from pydantic import BaseSettings
  
 class Settings(BaseSettings):
     #Configurações gerais da aplicação
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
     API_V1_STR: str = "/api/v1"
-    DB_URL: str = "postgresql+asyncpg://postgres:Alex01-lo@localhost:5432/faculdade"
+
+    @property
+    def DB_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     PROJECT_NAME: str = "Cursos API - FastAPI SQL Model"
 
     class Config():
+        env_file = ".env"
         case_sensitive = True
 
 settings: Settings = Settings()
